66f3fdd12d1e522fb3dc3352eb4f60bab7feb91d
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-07-10 13:51:38 +0200

    added spring-boot application

---
+++ coworking-system/src/main/java/ch/banyard/coworking_system/CoworkingSystemApplication.java
@@ -0,0 +1,13 @@
+package ch.banyard.coworking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworkingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoworkingSystemApplication.class, args);
+	}
+
+}
---
+++ coworking-system/src/test/java/ch/banyard/coworking_system/CoworkingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.banyard.coworking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
