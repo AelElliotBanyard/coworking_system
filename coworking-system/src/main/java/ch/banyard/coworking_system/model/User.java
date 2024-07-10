c1eddcbf9664fc3a83ed87ce70017719113df961
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-07-10 13:58:09 +0200

    created User model

---
+++ coworking-system/src/main/java/ch/banyard/coworking_system/model/User.java
@@ -0,0 +1,33 @@
+package ch.banyard.coworking_system.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@NotBlank
+	private String email;
+
+	@NotBlank
+	private String password;
+
+	@NotBlank
+	private String firstName;
+
+	@NotBlank
+	private String lastName;
+
+	private Boolean blocked;
+
+}
