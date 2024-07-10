1fb4a7ca29ed5be5ec3f682563a868aed1a89aa0
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-07-10 14:12:55 +0200

    Added Enums for Day and for Status

---
+++ coworking-system/src/main/java/ch/banyard/coworking_system/model/enums/Day.java
@@ -0,0 +1,5 @@
+package ch.banyard.coworking_system.model.enums;
+
+public enum Day {
+	FUll_DAY, HALF_DAY_MORNING, HALF_DAY_AFTERNOON
+}
---
+++ coworking-system/src/main/java/ch/banyard/coworking_system/model/enums/Status.java
@@ -0,0 +1,5 @@
+package ch.banyard.coworking_system.model.enums;
+
+public enum Status {
+	REQUESTED, CONFIRMED, DECLINED, CANCELLED
+}
