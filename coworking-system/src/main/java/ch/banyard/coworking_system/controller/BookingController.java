5a7fbde10c2704737f9e98b03a4b4709a3ecdc7c
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-07-11 13:30:47 +0200

    created Endpoints for bookings

--- coworking-system/src/main/java/ch/banyard/coworking_system/controller/BookingController.java
+++ coworking-system/src/main/java/ch/banyard/coworking_system/controller/BookingController.java
@@ -1,10 +1,103 @@
 package ch.banyard.coworking_system.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ch.banyard.coworking_system.model.Booking;
+import ch.banyard.coworking_system.model.CoworkingUser;
+import ch.banyard.coworking_system.model.dto.BookingDTO;
+import ch.banyard.coworking_system.model.enums.Roles;
+import ch.banyard.coworking_system.service.BookingService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/bookings")
 public class BookingController {
 
+	private final BookingService bookingService;
+
+	public BookingController(BookingService bookingService) {
+		this.bookingService = bookingService;
+	}
+
+	@GetMapping
+	public ResponseEntity<List<BookingDTO>> getBookings() {
+		List<BookingDTO> bookings = bookingService.getAllBookings();
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(bookings);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<BookingDTO> getBooking(Long id) {
+		try {
+			BookingDTO booking = bookingService.getBookingById(id);
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.contentType(MediaType.APPLICATION_JSON)
+					.body(booking);
+		} catch (Exception e) {
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.build();
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity<BookingDTO> createBooking(BookingDTO bookingDTO, @AuthenticationPrincipal CoworkingUser coworkingUser) {
+		try{
+		BookingDTO booking = bookingService.createBooking(bookingDTO, coworkingUser);
+		return ResponseEntity
+				.status(HttpStatus.CREATED)
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(booking);
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity
+					.status(HttpStatus.CONFLICT)
+					.build();
+		}
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<BookingDTO> updateBooking(@PathVariable Long id, @RequestBody BookingDTO bookingDTO, @AuthenticationPrincipal CoworkingUser coworkingUser) {
+		try {
+			if (coworkingUser.getRole() != Roles.ADMIN) {
+				return ResponseEntity
+						.status(HttpStatus.FORBIDDEN)
+						.build();
+			}
+			BookingDTO booking = bookingService.updateBooking(id, bookingDTO);
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.contentType(MediaType.APPLICATION_JSON)
+					.body(booking);
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.build();
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteBooking(@PathVariable Long id, @AuthenticationPrincipal CoworkingUser coworkingUser) {
+		try {
+			if (coworkingUser.getRole() != Roles.ADMIN) {
+				return ResponseEntity
+						.status(HttpStatus.FORBIDDEN)
+						.build();
+			}
+			bookingService.deleteBooking(id);
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.build();
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.build();
+		}
+	}
 }
--- coworking-system/src/main/java/ch/banyard/coworking_system/service/BookingService.java
+++ coworking-system/src/main/java/ch/banyard/coworking_system/service/BookingService.java
@@ -1,7 +1,59 @@
 package ch.banyard.coworking_system.service;
 
+import ch.banyard.coworking_system.model.Booking;
+import ch.banyard.coworking_system.model.CoworkingUser;
+import ch.banyard.coworking_system.model.dto.BookingDTO;
+import ch.banyard.coworking_system.model.dto.CoworkingUserDTO;
+import ch.banyard.coworking_system.model.mapper.BookingMapper;
+import ch.banyard.coworking_system.repository.BookingRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookingService {
+
+	private final BookingRepository bookingRepository;
+	private final BookingMapper bookingMapper;
+
+	public BookingService(BookingRepository bookingRepository, BookingMapper bookingMapper) {
+		this.bookingRepository = bookingRepository;
+		this.bookingMapper = bookingMapper;
+	}
+
+	public List<BookingDTO> getAllBookings() {
+		List<Booking> bookings = bookingRepository.findAll();
+		return bookingMapper.mapBookingsToDtoList(bookings);
+	}
+
+	public BookingDTO getBookingById(Long id) {
+		Booking booking = bookingRepository.findById(id).orElseThrow();
+		return bookingMapper.mapBookingToDTO(booking);
+	}
+
+	public BookingDTO createBooking(BookingDTO bookingDTO, CoworkingUser coworkingUser) {
+		Booking booking = bookingMapper.mapsBookingDtoToEntity(bookingDTO);
+		if  (bookingRepository.findById(booking.getId()).isPresent()) {
+			throw new IllegalArgumentException("Booking already exists");
+		}
+		booking.setCoworkingUser(coworkingUser);
+		bookingRepository.save(booking);
+		return bookingMapper.mapBookingToDTO(booking);
+	}
+
+	public BookingDTO updateBooking(Long id, BookingDTO bookingDTO) {
+		Booking booking = bookingMapper.mapsBookingDtoToEntity(bookingDTO);
+		if  (bookingRepository.findById(id).isEmpty()) {
+			throw new IllegalArgumentException("Booking does not exist");
+		}
+		bookingRepository.save(booking);
+		return bookingMapper.mapBookingToDTO(booking);
+	}
+
+	public void deleteBooking(Long id) {
+		if (bookingRepository.findById(id).isEmpty()) {
+			throw new IllegalArgumentException("Booking does not exist");
+		}
+		bookingRepository.deleteById(id);
+	}
 }
