a4918c9a42054796a252d6020c8b1a2f71fa44c5
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-07-11 13:19:01 +0200

    added Endpoints for rooms

--- coworking-system/src/main/java/ch/banyard/coworking_system/controller/RoomController.java
+++ coworking-system/src/main/java/ch/banyard/coworking_system/controller/RoomController.java
@@ -1,9 +1,109 @@
 package ch.banyard.coworking_system.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ch.banyard.coworking_system.model.CoworkingUser;
+import ch.banyard.coworking_system.model.dto.RoomDTO;
+import ch.banyard.coworking_system.model.enums.Roles;
+import ch.banyard.coworking_system.service.RoomService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/rooms")
 public class RoomController {
+
+	private final RoomService roomService;
+
+	public RoomController(RoomService roomService) {
+		this.roomService = roomService;
+	}
+
+	@GetMapping
+	public ResponseEntity<List<RoomDTO>> getRooms() {
+		List<RoomDTO> rooms = roomService.getAllRooms();
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(rooms);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<RoomDTO> getRoom(@PathVariable Long id) {
+		try {
+			RoomDTO room = roomService.getRoomById(id);
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.contentType(MediaType.APPLICATION_JSON)
+					.body(room);
+		} catch (Exception e) {
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.build();
+		}
+	}
+
+
+	@PostMapping
+	public ResponseEntity<RoomDTO> createRoom(@RequestBody @Valid RoomDTO roomDTO, @AuthenticationPrincipal CoworkingUser coworkingUser) {
+		try {
+			if (coworkingUser.getRole() != Roles.ADMIN) {
+				return ResponseEntity
+						.status(HttpStatus.FORBIDDEN)
+						.build();
+			}
+			RoomDTO room = roomService.createRoom(roomDTO);
+			return ResponseEntity
+					.status(HttpStatus.CREATED)
+					.contentType(MediaType.APPLICATION_JSON)
+					.body(room);
+		} catch (Exception e) {
+			return ResponseEntity
+					.status(HttpStatus.CONFLICT)
+					.build();
+		}
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<RoomDTO> updateRoom(@PathVariable Long id, @RequestBody @Valid RoomDTO roomDTO, @AuthenticationPrincipal CoworkingUser coworkingUser) {
+		try {
+			if (coworkingUser.getRole() != Roles.ADMIN) {
+				return ResponseEntity
+						.status(HttpStatus.FORBIDDEN)
+						.build();
+			}
+			RoomDTO room = roomService.updateRoom(id, roomDTO);
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.contentType(MediaType.APPLICATION_JSON)
+					.body(room);
+		} catch (Exception e) {
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.build();
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteRoom(@PathVariable Long id, @AuthenticationPrincipal CoworkingUser coworkingUser) {
+		try {
+			if (coworkingUser.getRole() != Roles.ADMIN) {
+				return ResponseEntity
+						.status(HttpStatus.FORBIDDEN)
+						.build();
+			}
+			roomService.deleteRoom(id);
+			return ResponseEntity
+					.status(HttpStatus.NO_CONTENT)
+					.build();
+		} catch (Exception e) {
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.build();
+		}
+	}
 }
--- coworking-system/src/main/java/ch/banyard/coworking_system/service/RoomService.java
+++ coworking-system/src/main/java/ch/banyard/coworking_system/service/RoomService.java
@@ -1,7 +1,56 @@
 package ch.banyard.coworking_system.service;
 
+import ch.banyard.coworking_system.model.Room;
+import ch.banyard.coworking_system.model.dto.RoomDTO;
+import ch.banyard.coworking_system.model.mapper.RoomMapper;
+import ch.banyard.coworking_system.repository.RoomRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RoomService {
+
+	private final RoomRepository roomRepository;
+	private final RoomMapper roomMapper;
+
+	public RoomService(RoomRepository roomRepository, RoomMapper roomMapper) {
+		this.roomRepository = roomRepository;
+		this.roomMapper = roomMapper;
+	}
+
+	public List<RoomDTO> getAllRooms() {
+		List<Room> rooms = roomRepository.findAll();
+		return roomMapper.mapRoomsToDtoList(rooms);
+	}
+
+	public RoomDTO getRoomById(Long id) {
+		Room room = roomRepository.findById(id).orElseThrow();
+		return roomMapper.mapRoomToDTO(room);
+	}
+
+	public RoomDTO createRoom(RoomDTO roomDTO) {
+		Room room = roomMapper.mapRoomDtoToEntity(roomDTO);
+		if  (roomRepository.findById(room.getId()).isPresent()) {
+			throw new IllegalArgumentException("Room already exists");
+		}
+		roomRepository.save(room);
+		return roomMapper.mapRoomToDTO(room);
+	}
+
+	public RoomDTO updateRoom(Long id, RoomDTO roomDTO) {
+		Room room = roomMapper.mapRoomDtoToEntity(roomDTO);
+		if  (roomRepository.findById(id).isEmpty()) {
+			throw new IllegalArgumentException("Room does not exist");
+		}
+		roomRepository.save(room);
+		return roomMapper.mapRoomToDTO(room);
+	}
+
+	public void deleteRoom(Long id) {
+		if (roomRepository.findById(id).isEmpty()) {
+			throw new IllegalArgumentException("Room does not exist");
+		}
+		roomRepository.deleteById(id);
+	}
 }
