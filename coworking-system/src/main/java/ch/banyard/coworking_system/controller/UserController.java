7ba025ba4467ae8e30c1345d01e6ef6263a261cd
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-07-12 14:21:17 +0200

    fixed errors in user controller

--- coworking-system/src/main/java/ch/banyard/coworking_system/controller/UserController.java
+++ coworking-system/src/main/java/ch/banyard/coworking_system/controller/UserController.java
@@ -1,6 +1,7 @@
 package ch.banyard.coworking_system.controller;
 
 import ch.banyard.coworking_system.model.CoworkingUser;
+import ch.banyard.coworking_system.model.request.RequestUser;
 import ch.banyard.coworking_system.model.dto.CoworkingUserDTO;
 import ch.banyard.coworking_system.model.enums.Roles;
 import ch.banyard.coworking_system.service.UserService;
@@ -16,6 +17,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,9 +30,11 @@ import java.util.Objects;
 public class UserController {
 
 	private final UserService userService;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	public UserController(UserService userService) {
+	public UserController(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.userService = userService;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
 	@GetMapping
@@ -42,11 +47,18 @@ public class UserController {
 					content = @Content)
 	})
 	public ResponseEntity<List<CoworkingUserDTO>> getUsers() {
-		List<CoworkingUserDTO> users = userService.getAllUsers();
-		return ResponseEntity
-				.status(HttpStatus.OK)
-				.contentType(MediaType.APPLICATION_JSON)
-				.body(users);
+		CoworkingUser coworkingUser = userService.getUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+		if (coworkingUser.getRole() != Roles.ADMIN) {
+			return ResponseEntity
+					.status(HttpStatus.FORBIDDEN)
+					.build();
+		} else {
+			List<CoworkingUserDTO> users = userService.getAllUsers();
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.contentType(MediaType.APPLICATION_JSON)
+					.body(users);
+		}
 	}
 
 	@GetMapping("/{id}")
@@ -60,20 +72,30 @@ public class UserController {
 			@ApiResponse(responseCode = "403", description = "Forbidden",
 					content = @Content)
 	})
-	public ResponseEntity<CoworkingUserDTO> getUser(@Parameter(description = "id of user to get") @PathVariable String id, @AuthenticationPrincipal CoworkingUser coworkingUser) {
+	public ResponseEntity<CoworkingUserDTO> getUser(@Parameter(description = "id of user to get") @PathVariable String id) {
 		try {
+
+			CoworkingUser coworkingUser = userService.getUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
 			if (coworkingUser.getRole() != Roles.ADMIN) {
 				if (!Objects.equals(coworkingUser.getId(), Long.valueOf(id))) {
 					return ResponseEntity
 							.status(HttpStatus.FORBIDDEN)
 							.build();
+				} else {
+					CoworkingUserDTO user = userService.getUserById(Long.valueOf(id));
+					return ResponseEntity
+							.status(HttpStatus.OK)
+							.contentType(MediaType.APPLICATION_JSON)
+							.body(user);
+
 				}
+			} else {
+				CoworkingUserDTO user = userService.getUserById(Long.valueOf(id));
+				return ResponseEntity
+						.status(HttpStatus.OK)
+						.contentType(MediaType.APPLICATION_JSON)
+						.body(user);
 			}
-			CoworkingUserDTO user = userService.getUserById(Long.valueOf(id));
-			return ResponseEntity
-					.status(HttpStatus.OK)
-					.contentType(MediaType.APPLICATION_JSON)
-					.body(user);
 		} catch (Exception e) {
 			return ResponseEntity
 					.status(HttpStatus.NOT_FOUND)
@@ -90,9 +112,10 @@ public class UserController {
 			@ApiResponse(responseCode = "409", description = "User already exists",
 					content = @Content)
 	})
-	public ResponseEntity<CoworkingUserDTO> createUser(@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "new user") @RequestBody CoworkingUserDTO userDTO) {
+	public ResponseEntity<CoworkingUserDTO> createUser(@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "new user") @RequestBody RequestUser requestUser) {
 		try {
-			CoworkingUserDTO user = userService.createUser(userDTO);
+			requestUser.setPassword(bCryptPasswordEncoder.encode(requestUser.getPassword()));
+			CoworkingUserDTO user = userService.createUser(requestUser);
 			return ResponseEntity
 					.status(HttpStatus.CREATED)
 					.contentType(MediaType.APPLICATION_JSON)
@@ -115,20 +138,98 @@ public class UserController {
 			@ApiResponse(responseCode = "403", description = "Forbidden",
 					content = @Content)
 	})
-	public ResponseEntity<CoworkingUserDTO> updateUser(@Parameter(description = "id of user to be updated") @PathVariable String id,@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "user to update") @RequestBody CoworkingUserDTO userDTO, @AuthenticationPrincipal CoworkingUser coworkingUser) {
+	public ResponseEntity<CoworkingUserDTO> updateUser(@Parameter(description = "id of user to be updated") @PathVariable String id,@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "user to update") @RequestBody CoworkingUserDTO userDTO) {
 		try {
+			CoworkingUser coworkingUser = userService.getUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+			if (!Objects.equals(coworkingUser.getId(), Long.valueOf(id))) {
 			if (coworkingUser.getRole() != Roles.ADMIN) {
-				if (!Objects.equals(coworkingUser.getId(), Long.valueOf(id))) {
 					return ResponseEntity
 							.status(HttpStatus.FORBIDDEN)
 							.build();
+				} else {
+				CoworkingUserDTO oldUser = userService.getUserById(Long.valueOf(id));
+				CoworkingUser newUser = new CoworkingUser();
+				newUser.setId(Long.valueOf(id));
+				if (userDTO.password() == null) {
+					newUser.setPassword(oldUser.password());
+				} else {
+					newUser.setPassword(userDTO.password());
 				}
+				if (userDTO.firstName() == null) {
+					newUser.setFirstName(oldUser.firstName());
+				} else {
+					newUser.setFirstName(userDTO.firstName());
+				}
+				if (userDTO.lastName() == null) {
+					newUser.setLastName(oldUser.lastName());
+				} else {
+					newUser.setLastName(userDTO.lastName());
+				}
+				if (userDTO.username() == null) {
+					newUser.setUsername(oldUser.username());
+				} else {
+					newUser.setUsername(userDTO.username());
+				}
+				if (userDTO.blocked() != null && coworkingUser.getRole() == Roles.ADMIN){
+					newUser.setBlocked(userDTO.blocked());
+				}
+				else{
+					newUser.setBlocked(oldUser.blocked());
 			}
-			CoworkingUserDTO user = userService.updateUser(Long.valueOf(id), userDTO);
-			return ResponseEntity
-					.status(HttpStatus.OK)
-					.contentType(MediaType.APPLICATION_JSON)
-					.body(user);
+				if (userDTO.role() != null && coworkingUser.getRole() == Roles.ADMIN){
+					newUser.setRole(userDTO.role());
+				}
+				else{
+					newUser.setRole(oldUser.role());
+				}
+					CoworkingUserDTO user = userService.updateUser(Long.valueOf(id), newUser);
+					return ResponseEntity
+							.status(HttpStatus.OK)
+							.contentType(MediaType.APPLICATION_JSON)
+							.body(user);
+			}
+			} else {
+				CoworkingUserDTO oldUser = userService.getUserById(Long.valueOf(id));
+				CoworkingUser newUser = new CoworkingUser();
+				newUser.setId(Long.valueOf(id));
+				if (userDTO.password() == null) {
+					newUser.setPassword(oldUser.password());
+				} else {
+					newUser.setPassword(userDTO.password());
+				}
+				if (userDTO.firstName() == null) {
+					newUser.setFirstName(oldUser.firstName());
+				} else {
+					newUser.setFirstName(userDTO.firstName());
+				}
+				if (userDTO.lastName() == null) {
+					newUser.setLastName(oldUser.lastName());
+				} else {
+					newUser.setLastName(userDTO.lastName());
+				}
+				if (userDTO.username() == null) {
+					newUser.setUsername(oldUser.username());
+				} else {
+					newUser.setUsername(userDTO.username());
+				}
+				if (userDTO.blocked() != null && coworkingUser.getRole() == Roles.ADMIN){
+					newUser.setBlocked(userDTO.blocked());
+				}
+				else{
+					newUser.setBlocked(oldUser.blocked());
+				}
+				if (userDTO.role() != null && coworkingUser.getRole() == Roles.ADMIN){
+					newUser.setRole(userDTO.role());
+				}
+				else{
+					newUser.setRole(oldUser.role());
+				}
+				CoworkingUserDTO user = userService.updateUser(Long.valueOf(id), newUser);
+				return ResponseEntity
+						.status(HttpStatus.OK)
+						.contentType(MediaType.APPLICATION_JSON)
+						.body(user);
+			}
 		} catch (Exception e) {
 			return ResponseEntity
 					.status(HttpStatus.NOT_FOUND)
@@ -146,19 +247,27 @@ public class UserController {
 			@ApiResponse(responseCode = "403", description = "Forbidden",
 					content = @Content)
 	})
-	public ResponseEntity<Void> deleteUser(@Parameter(description = "id of user to be deleted") @PathVariable String id, @AuthenticationPrincipal CoworkingUser coworkingUser) {
+	public ResponseEntity<Void> deleteUser(@Parameter(description = "id of user to be deleted") @PathVariable String id) {
 		try {
-			if (coworkingUser.getRole() != Roles.ADMIN) {
-				if (!Objects.equals(coworkingUser.getId(), Long.valueOf(id))) {
+			CoworkingUser coworkingUser = userService.getUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+			if (!Objects.equals(coworkingUser.getId(), Long.valueOf(id))) {
+				if (coworkingUser.getRole() != Roles.ADMIN) {
 					return ResponseEntity
 							.status(HttpStatus.FORBIDDEN)
 							.build();
+				} else {
+					userService.deleteUser(Long.valueOf(id));
+					return ResponseEntity
+							.status(HttpStatus.NO_CONTENT)
+							.build();
+
 				}
+			} else {
+				userService.deleteUser(Long.valueOf(id));
+				return ResponseEntity
+						.status(HttpStatus.NO_CONTENT)
+						.build();
 			}
-			userService.deleteUser(Long.valueOf(id));
-			return ResponseEntity
-					.status(HttpStatus.NO_CONTENT)
-					.build();
 		} catch (Exception e) {
 			return ResponseEntity
 					.status(HttpStatus.NOT_FOUND)
